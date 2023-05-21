5cc98323179a7fea5ee19dca2a537d86b1208b60
Twizzay <gandalf@mylegendary.quest>
2023-05-21 00:10:09 -0700

    I am probably going overboard with this but I am scraping for almost, like every sprite in the game. I might rethink this in the morning
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   scraper.py

--- scraper.py
+++ scraper.py
@@ -1,28 +1,77 @@
 #! /usr/bin/env python3
+import requests, os
 from bs4 import BeautifulSoup
-import requests
+from pathlib import Path
 
-url = 'https://cavestory.fandom.com/wiki/Category:Characters'
-http = requests.get(url)
+p = Path(__file__).parents[1]
 
-soup = BeautifulSoup(http.text, 'html.parser')
+if not os.path.exists(f'{p}/scraped'):
+    os.mkdir(f'{p}/scraped')
+else:
+    print('Scrape Directory already exists. Overwrite? Y/n')
+    if input() == 'y':
+        pass
+    else:
+        exit()
 
-page_members = soup.find('div', class_='category-page__members')
-page_member_categories = page_members.find_all('div', class_='category-page__members-wrapper')
-
-page_urls = set()
-for i in page_member_categories:
-    page_anchors = i.select('ul > li > a')
-    for a in page_anchors:
-        if not 'Category' in a.get('href'):
-            page_urls.add(f'https://cavestory.fandom.com{a.get("href")}')
-
-
-for url in page_urls:
-    print(f'Page scraped: {url}')
-    print('')
+def get_pages(url):
     http = requests.get(url)
     soup = BeautifulSoup(http.text, 'html.parser')
-    sprite = soup.find('img', class_='pi-image-thumbnail')
-    sprite_src = sprite.get('src')
-    print(f'Sprite URL: {sprite_src}')
+
+    page_members = soup.find('div', class_='category-page__members')
+    page_member_categories = page_members.find_all('div', class_='category-page__members-wrapper')
+
+    page_urls = set()
+    for i in page_member_categories:
+        page_anchors = i.select('ul > li > a')
+        for a in page_anchors:
+            if not 'Category' in a.get('href'):
+                page_urls.add(f'https://cavestory.fandom.com{a.get("href")}')
+    return page_urls
+
+def get_sprites(page_urls):
+    for url in page_urls:
+        print(f'\n\nPage scraped: {url}')
+        http = requests.get(url)
+        soup = BeautifulSoup(http.text, 'html.parser')
+        header2 = soup.find_all('h2')
+        #This loop is currently overscraping.
+        #There are multiple h2 elements on each page.
+        # So when the first If statement returns False,
+        # the second/third elif statement scrapes content anyway.
+        for h2 in header2:
+            if h2.text == 'Sprites':
+                figure = h2.find_next('figure')
+                sprite = figure.img
+                break
+            elif (soup.find('img', class_='pi-image-thumbnail')):
+                sprite = soup.find('img', class_='pi-image-thumbnail')
+                break
+            elif (soup.find('table', class_='infobox')):
+                table = soup.find('table', class_='infobox')
+                sprite = table.img
+
+        sprite_src = sprite.get('src')
+        sprite_url.add(sprite_src)
+        print(f'Sprite URL: {sprite_src}')
+
+character_index_url = 'https://cavestory.fandom.com/wiki/Category:Characters'
+enemy_index_url = 'https://cavestory.fandom.com/wiki/Category:Enemies'
+character_page_urls = set(get_pages(character_index_url))
+enemy_page_urls = set(get_pages(enemy_index_url))
+
+sprite_url = set()
+unsuccesful_page_url = set()
+get_sprites(enemy_page_urls)
+get_sprites(character_page_urls)
+
+for url in sprite_url:
+    with open(f'{p}/scraped/sprite_url.csv', 'a') as a:
+        a.write(f'{url},\n')
+
+    with open(f'{p}/scraped/sprites.html', 'a') as a:
+        a.write(f'<img src="{url}"><br>')
+
+for url in unsuccesful_page_url:
+    with open(f'{p}/scraped/unsuccesful_sprite_url.csv', 'a') as a:
+        a.write(f'{url},\n')
